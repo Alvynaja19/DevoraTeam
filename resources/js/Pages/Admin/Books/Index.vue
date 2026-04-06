d435712c69506aac30d327fe14f4664b33ec8c43
ferdiodwi <ferdiodwi12@gmail.com>
2026-04-07 00:10:34 +0700

    fix: update filter binding from sort to availability in book index page

--- resources/js/Pages/Admin/Books/Index.vue
+++ resources/js/Pages/Admin/Books/Index.vue
@@ -86,7 +86,7 @@
             <option value="habis">Habis</option>
           </select>
 
-          <select v-model="filters.sort" @change="applyFilter"
+          <select v-model="filters.availability" @change="applyFilter"
             class="w-full md:w-auto px-4 py-2 text-sm rounded-lg border border-gray-300 bg-white text-gray-900 focus:ring-2 focus:ring-emerald-500">
             <option value="">Urutkan: Terbaru</option>
             <option value="terpopuler">Terpopuler</option>
